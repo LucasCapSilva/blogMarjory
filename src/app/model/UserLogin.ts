bc0b7b50fd76bb89db601e4f3b9d2675551748e4
unknown <lcapelotto@gmail.com>
2022-06-14 18:16:23 -0300

    1.0

--- src/app/model/UserLogin.ts
+++
@@ -1,9 +0,0 @@
-export class UserLogin {
-    public id:number;
-    public nome: string;
-    public emailUsuario: string;
-    public senha: string;
-    public foto: string;
-    public token: string;
-    public tipo: string;
-}
